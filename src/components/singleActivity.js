2beaf10f2fbd903097daad2a88a38746675a7d2c
Emelie M <emelie.moya@aol.com>
2021-11-06 13:41:20 -0700

    last stretch

--- src/components/singleActivity.js
+++ src/components/singleActivity.js
@@ -1,14 +1,95 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
-import { deleteActivity, editActivity } from "../api";
+import { deleteRoutineActivity, editActivity } from "../api";
 
-const singleActivity = ({ activity }) => {
+const SingleActivity = ({ activity }) => {
   const history = useHistory();
 
   const activityId = activity.id;
 
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+
+  const [isActive, setActive] = useState(false);
+  const editToggle = () => {
+    return setActive(!isActive);
+  };
+
+  const [deleteActive, setDeleteActive] = useState(false);
+  const deleteToggle = () => {
+    return setDeleteActive(!deleteActive);
+  };
+  const [certainDelete, setCertainDelete] = useState(false);
+
+  return (
+    <div>
+      <button onClick={editToggle}>Edit?</button>
+      <div className={isActive ? "singleActivity" : "hidden"}>
+        <form
+          name="editRoutine"
+          onSubmit={async (event) => {
+            event.preventDefault();
+
+            try {
+              const results = await editActivity(name, description, activityId);
+              return results;
+            } catch (error) {
+              throw error;
+            }
+          }}
+        >
+          <fieldset>
+            <label>Change name?</label>
+            <input
+              name="editName"
+              type="text"
+              value={name}
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
+          </fieldset>
+
+          <fieldset>
+            <label>Change Description?</label>
+            <input
+              name="editDesc"
+              type="text"
+              value={name}
+              onChange={(event) => {
+                setDescription(event.target.value);
+              }}
+            />
+          </fieldset>
+        </form>
+      </div>
+
+      <button onClick={deleteToggle}>Delete?</button>
+      <div className={deleteActive ? null : "hidden"}>
+        <fieldset className="fieldset">
+          <label>Are you sure you want to delete?</label>
+
+          <input
+            id="certainDelete"
+            type="checkbox"
+            value={certainDelete}
+            onChange={() => {
+              setCertainDelete(!certainDelete);
+            }}
+          />
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              certainDelete ? deleteRoutineActivity(activityId) : null;
+              history.push("/");
+            }}
+          >
+            Complete Deletion
+          </button>
+        </fieldset>
+      </div>
+    </div>
+  );
 };
 
-export default singleActivity;
+export default SingleActivity;
