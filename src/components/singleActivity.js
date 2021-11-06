fe6228abdcc9c5ac0f2f713d6255beb742e2890a
Emelie M <emelie.moya@aol.com>
2021-11-06 10:07:25 -0700

    Uhhh

--- src/components/singleActivity.js
+++ src/components/singleActivity.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
-import { updateRoutine, deleteRoutine } from "../api";
+import { deleteActivity, editActivity } from "../api";
 
 const singleActivity = ({ activity }) => {
   const history = useHistory();
